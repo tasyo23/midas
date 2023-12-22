a3cd071b81184ba9adced15fa6e37a5da531d175
pulna <natasyo23@gmail.com>
2023-12-22 22:37:26 +0300

    feat:mobile

--- src/js/index.js
+++ src/js/index.js
@@ -29,30 +29,26 @@ $(".slider").slick({
   slidesToScroll: 3,
   variableWidth: true,
   prevArrow: `<button type="button" class="slider__arrow slider__arrow_next" aria-label="Следующие продукты"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></button>`,
-  nextArrow: `<button type="button" class="slider__arrow slider__arrow_prev" aria-label="Предыдущие продукты"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></button>`
+  nextArrow: `<button type="button" class="slider__arrow slider__arrow_prev" aria-label="Предыдущие продукты"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></button>`,
 
-  // // the magic
-  // responsive: [{
-  //
-  //     breakpoint: 1024,
-  //     settings: {
-  //         slidesToShow: 3,
-  //         infinite: true
-  //     }
-  //
-  // }, {
-  //
-  //     breakpoint: 600,
-  //     settings: {
-  //         slidesToShow: 2,
-  //         dots: true
-  //     }
-  //
-  // }, {
-  //
-  //     breakpoint: 300,
-  //     settings: "unslick" // destroys slick
-  //
-  // }
-  // ]
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+    }
+  ]
 });
